d5b2dea889b9eee54fba5350bb211ea28d325d44
Udaya Bhasker Mattupalli <uday1005@gmail.com>
2019-10-29 14:18:02 -0700

    remove sha

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker build -t uday1005/multi-client:latest -t uday1005/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t uday1005/multi-server:latest -t uday1005/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t uday1005/multi-worker:latest -t uday1005/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t uday1005/multi-client:latest -t uday1005/multi-client:123456uday -f ./client/Dockerfile ./client
+docker build -t uday1005/multi-server:latest -t uday1005/multi-server:123456uday -f ./server/Dockerfile ./server
+docker build -t uday1005/multi-worker:latest -t uday1005/multi-worker:123456uday -f ./worker/Dockerfile ./worker
 docker push uday1005/multi-client
 docker push uday1005/multi-server
 docker push uday1005/multi-worker
-docker push uday1005/multi-client:$SHA
-docker push uday1005/multi-server:$SHA
-docker push uday1005/multi-worker:$SHA
+docker push uday1005/multi-client:123456uday
+docker push uday1005/multi-server:123456uday
+docker push uday1005/multi-worker:123456uday
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=uday1005/multi-server:$SHA
-kubectl set image deployments/client-deployment client=uday1005/multi-clientL$SHA
-kubectl set image deployment/worker-deployment worker=uday1005/multi-client:$SHA
+kubectl set image deployments/server-deployment server=uday1005/multi-server:123456uday
+kubectl set image deployments/client-deployment client=uday1005/multi-clientL123456uday
+kubectl set image deployment/worker-deployment worker=uday1005/multi-client:123456uday
 
